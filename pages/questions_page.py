aef20075bde4cfe3316c241b2f2e9e402f95f7d8
MooningFlux <MooningFlux@users.noreply.github.com>
2025-05-20 16:09:36 +0000

    Automated deployment: Tue May 20 16:09:36 UTC 2025 03cf2d03a386d5af6f51348bda3d12554a347728

--- pages/questions_page.py
+++
@@ -1,7 +0,0 @@
-from playwright.sync_api import Page
-from pages.base_page import BasePage
-
-#class definitions cannot be empty, but if you for some reason have a class definition with no content, 
-# put in the pass statement to avoid getting an error.
-class QuestionsPage(BasePage):
-    pass
